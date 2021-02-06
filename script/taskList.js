17f818ab03a5bb016172dcc9ec7e71bc63d777d3
Hjs-dev <hitham1123@gmail.com>
2021-02-07 02:03:14 +0300

    practiec js

--- script/taskList.js
+++ script/taskList.js
@@ -11,6 +11,10 @@ laodEventListeners()
 function laodEventListeners() {
     // add task event 
     form.addEventListener('submit', addTask)
+    // Remove task event 
+    taskList.addEventListener('click', removeTask)
+    // Clear Task event
+    clearBtn.addEventListener('click', clearTasks)
 }
 
 function addTask(e) {
@@ -21,6 +25,45 @@ function addTask(e) {
     const li = document.createElement('li')
     // Add class name 
     li.className = 'collection-item'
+    // Create text node and append to li 
+    li.appendChild(document.createTextNode(taskInput.value))
+    // Create new link element 
+    const link = document.createElement('a')
+    // Add class anme
+    link.className = 'delete-item secondary-content'
+    // Add icon html 
+    link.innerHTML = '<i class="fa fa-remove"></i>'
+    // Append the link to li 
+    li.appendChild(link)
+    // Append li to ul 
+    taskList.appendChild(li)
+    // Cleare input 
+    taskInput.value = ''
+
+console.log(li)
 
     e.preventDefault()
+}
+
+// Remove task
+function removeTask(e) {
+    if (e.target.parentElement.classList.contains('delete-item')) {
+        if (confirm('Are you sure?')) {
+            
+            e.target.parentElement.parentElement.remove()
+        }
+       
+    }
+    
+
+}
+
+
+function clearTasks() {
+    // taskList.innerHTML = ''
+
+    // Fasster
+    while (taskList.firstChild) {
+        taskList.removeChild(taskList.firstChild)
+    }
 }
