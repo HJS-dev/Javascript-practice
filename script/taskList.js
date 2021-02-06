b14d18553711f0fc6a9a028d37f2af2cf040ddf0
Hjs-dev <hitham1123@gmail.com>
2021-02-07 00:42:10 +0300

    practiec js

--- script/taskList.js
+++ script/taskList.js
@@ -2,3 +2,25 @@
 const form = document.querySelector('#task-form')
 const taskList = document.querySelector('.collection')
 const clearBtn = document.querySelector('.clear-tasks')
+const filter = document.querySelector('#filter')
+const taskInput = document.querySelector('#task')
+
+// Load all event listeners
+laodEventListeners()
+
+function laodEventListeners() {
+    // add task event 
+    form.addEventListener('submit', addTask)
+}
+
+function addTask(e) {
+    if (taskInput.value === '') {
+        alert('Add a task')
+    }
+    // Creat li element
+    const li = document.createElement('li')
+    // Add class name 
+    li.className = 'collection-item'
+
+    e.preventDefault()
+}
