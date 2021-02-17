02363f0feef9e8fc09fd338f15c8d322ef715d62
Hjs-dev <hitham1123@gmail.com>
2021-02-17 23:51:58 +0300

    practice

--- script/Loan-C.js
+++ script/Loan-C.js
@@ -11,15 +11,36 @@ function calculateResults(e) {
     const totalInterest = document.getElementById('total-interest')
 
     const principal = parseFloat(amount.value)
-    const calculatedInterest = praseFloat(interest.value) / 100 / 12
+    const calculatedInterest = parseFloat(interest.value) / 100 / 12
     const calculatedPayment = parseFloat(years.value) * 12
 
-    const x = Math.pow()
-
-
-
-
-
+    const x = Math.pow(1 + calculatedInterest, calculatedPayment)
+    const monthly = (principal * x * calculatedInterest) / (x - 1)
+    
+    if (isFinite(monthly)) {
+        monthlyPayment.value = monthly.toFixed(2)
+        totalPayment.value = (monthly * calculatedPayment).toFixed(2)
+        totalInterest.value = ((monthly * calculatedPayment) - principal).toFixed(2)
+    }
+    else {
+       errorMessage('Please check your number')
+        }
 
     e.preventDefault()
+}
+
+
+function errorMessage(error) {
+    // create a div
+    const errorDiv = document.createElement('div')
+
+    const card = document.querySelector('.card')
+    const heading = document.querySelector('.heading')
+
+    errorDiv.className = 'alert alert-danger'
+    errorDiv.appendChild(document.createTextNode(error))
+
+
+
+    
 }
